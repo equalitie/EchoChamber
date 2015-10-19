fb1374a8346194f21cdf219502fb18d27c7cbf22
Zack Mullaly <redwire@riseup.net>
2015-10-18 22:50:34 -0400

    Build all source files and add test call

--- build.sh
+++ build.sh
@@ -1,9 +1,14 @@
 #! /usr/bin/env sh
 
-SOURCE_FILES="src/chamber.go"
+SOURCE_FILES="src/chamber.go \
+    src/clients.go \
+    src/messagetypes.go"
 
 echo "Formatting go files."
 for file in `ls src/*.go`; do
+  go fmt $file;
+done
+for file in `ls src/tests/*.go`; do
   go fmt $file;
 done
 
@@ -11,6 +16,12 @@ echo ""
 echo "Installing dependencies."
 echo "github.com/gorilla/mux"
 go get github.com/gorilla/mux
-echo ""
 
+echo ""
+cd src/
+echo "Running tests."
+go test
+cd ../
+
+echo ""
 go build $SOURCE_FILES && echo "Compiled successfully."
