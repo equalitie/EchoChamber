522e02d0f5b643429be18e92bb0289edd8bef6f1
Chrrrles Paul <chrrrles@users.noreply.github.com>
2016-06-16 21:59:57 +0800

    fixing annoying typo

--- echochamber/test/latency.py
+++ echochamber/test/latency.py
@@ -3,7 +3,7 @@ from connection import ConnectionTest
 class LatencyTest(ConnectionTest):
     def _setup_clients(self):
         for n in range(len(self.test_data["clients"])):
-            client_data = self.test_data["clients"[n]]
+            client_data = self.test_data["clients"][n]
             client_data["port"] = 5223 + n
             self.clients.append(Client(client_data, self.config, self.debug))
             self._adduser(client_data)
