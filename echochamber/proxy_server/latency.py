274174eb1965818ab661480e8faa6d647297dc19
Chrrrles <charles.paul@gmail.com>
2016-06-24 07:39:28 +0000

    fixing message queue to send to correct sockets

--- echochamber/proxy_server/latency.py
+++ echochamber/proxy_server/latency.py
@@ -5,20 +5,28 @@ class LatencyProxyServer(BaseProxyServer):
     def __init__(self, host, port, fhost, fport, latency=0):
         super(self.__class__, self).__init__(host, port, fhost, fport)
         self.latency = latency / 1000.0
-        self.queue = []
+        self.queue = {}
+        self.forward = None
+        self.clientsock = None
 
     def communicate(self):
         super(self.__class__, self).communicate()
         now = time.time() 
-        pop = []
-        for n in range(len(self.queue)):
-            data = self.queue[n]
-            if data[1] <= now:
-                self.channel[self.s].send(data[0])
-                pop.append(n)
-        for n in sorted(pop, reverse=True):
-            del self.queue[n]
+        pop = {}
+        for s,q in self.queue.items(): # a distinct queue for each channel
+            pop[s] = []
+            for n in range(len(q)):
+                data = q[n]
+                if data[1] <= now:
+                    self.channel[s].send(data[0])
+                    pop[s].append(n)
+        for s,p in pop.items():
+            for n in sorted(p, reverse=True):
+                del self.queue[s][n]
 
     def on_recv(self):
+        if self.s not in self.queue.keys():
+            self.queue[self.s] = []
         future = time.time() + self.latency
-        self.queue.append((self.data, future))
+        print self.data
+        self.queue[self.s].append((self.data, future))
