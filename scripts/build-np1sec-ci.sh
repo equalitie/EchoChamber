527c95c137c4a4c071a5a32d470e7f39deae1917
Donncha O'Cearbhaill <donncha@donncha.is>
2016-12-02 12:33:59 +0100

    Add config to run EchoChamber on CircleCI

---
+++ scripts/build-np1sec-ci.sh
@@ -0,0 +1,9 @@
+set -x
+set -e
+if [ ! -e np1sec/jabberite ]; then
+  sudo apt-get install -y cmake libgcrypt-dev libglib2.0-dev libreadline-dev libpurple-dev
+  git clone https://github.com/equalitie/np1sec.git && cd np1sec
+  cmake .
+  make
+  sudo make install
+fi
