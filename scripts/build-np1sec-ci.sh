2ff87d32d8548741d4b733c629df5e041b05e27d
Donncha O'Cearbhaill <donncha@donncha.is>
2016-12-02 14:39:02 +0100

    Checkout the correct np1sec branch on CI

--- scripts/build-np1sec-ci.sh
+++ scripts/build-np1sec-ci.sh
@@ -3,6 +3,7 @@ set -e
 if [ ! -e np1sec/jabberite ]; then
   sudo apt-get install -y cmake libgcrypt20-dev libglib2.0-dev libreadline-dev libpurple-dev
   git clone https://github.com/equalitie/np1sec.git && cd np1sec
+  git checkout invites
   cmake .
   make
 fi
