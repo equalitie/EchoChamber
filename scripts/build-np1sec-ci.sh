adb4f71d6a2f2816ab6abe51ac0d179105ab201b
Donncha O'Cearbhaill <donncha@donncha.is>
2016-12-02 14:01:24 +0100

    Install the correct version of libgcrypt

--- scripts/build-np1sec-ci.sh
+++ scripts/build-np1sec-ci.sh
@@ -1,7 +1,7 @@
 set -x
 set -e
 if [ ! -e np1sec/jabberite ]; then
-  sudo apt-get install -y cmake libgcrypt-dev libglib2.0-dev libreadline-dev libpurple-dev
+  sudo apt-get install -y cmake libgcrypt20-dev libglib2.0-dev libreadline-dev libpurple-dev
   git clone https://github.com/equalitie/np1sec.git && cd np1sec
   cmake .
   make
